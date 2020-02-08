f1f4a81ba5aed5390c207f019c2ade7321131d2e
Greenleaf20 <archana.2098@gmail.com>
2020-02-08 09:29:50 +0530

    Add qr generator

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Components/Login';
 import Welcome from './Components/Welcome';
+import Checkin from './Components/Checkin';
 
 
 const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Checkin" component={Checkin} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Details" component={Welcome} />
       </Stack.Navigator>
